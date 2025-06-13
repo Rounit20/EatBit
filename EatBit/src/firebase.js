30261f1a610de595d006f3dc1e86862a51b188a3
Rounit20 <rounitsinha11@gmail.com>
2025-06-13 23:07:21 +0530

    commit

--- EatBit/src/firebase.js
+++ EatBit/src/firebase.js
@@ -4,6 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";  // ✅ Import Storage
 
 const firebaseConfig = {
+    apiKey: "AIzaSyCpGI_nv1eBwKHc99wmiF48ogOef5UYLzc",
+    authDomain: "eatbit-adbda.firebaseapp.com",
+    projectId: "eatbit-adbda",
+    storageBucket: "eatbit-adbda.appspot.com",  // ✅ Ensure storage is set up
+    messagingSenderId: "143894279429",
+    appId: "1:143894279429:web:a67ddaf9c6e10bddfdf36c",
+    measurementId: "G-0EJT77RCF4"
   
 };
 
